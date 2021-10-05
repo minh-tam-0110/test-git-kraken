3dee200b2e84eca3120c27cfa6e432ea81a15f70
thuan <thuanntv013@gmail.com>
2021-10-05 16:59:18 +0700

    command

---
+++ html/command.js
@@ -0,0 +1,14 @@
+href == 'https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments'
+
+/**
+ * MyMethod
+ * * Important infomation is highlighted
+ * ! Deprecated method, do not use
+ * ? Should this mathod be exposed in the public API
+ * TODO refactor this method so that it conforms to the API
+ * @param myPraram The parameter for this method
+ */
+
+//* this is a ...
+//? This is a ...
+//TODO: Create some code
